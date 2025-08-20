3991b49b26bb59a915395996adb63b0ef667385c
Rado Faletič <radofaletic@gmail.com>
2025-08-20 14:34:22 +1000

    Updated to use GitHub repository

---
+++ README.txt
@@ -0,0 +1,36 @@
+=== COVERSE Lens search ===
+Contributors: Rado Faletič
+Tags: COVERSE, Lens
+Requires at least: 6.2
+Tested up to: 6.2
+Stable tag: 1.1.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Wordpress shortcode to add a custom Lens search feature based on COVERSE’s custom database of COVID-19 vaccine adverse event research.
+
+== Description ==
+
+Wordpress shortcode to add a custom Lens search feature based on COVERSE’s custom database of COVID-19 vaccine adverse event research.
+
+== Changelog ==
+= 1.1.3 =
+* Changed update method to GitHub repository.
+= 1.1.2 =
+* Fixed submit button.
+= 1.1.1 =
+* CSS fixes.
+= 1.1.0 =
+* Added attribution shortcode.
+= 1.0.5 =
+* Fixed spacing.
+= 1.0.4 =
+* Fixed font sizes.
+= 1.0.3 =
+* Fixed display issues.
+= 1.0.2 =
+* Fixed minor bugs.
+= 1.0.1 =
+* Fixed minor bugs.
+= 1.0.0 =
+* Initial version.
--- coverse-lens-search.php
+++ coverse-lens-search.php
@@ -10,7 +10,7 @@
   * @wordpress-plugin
   * Plugin Name:       COVERSE Lens search
   * Description:       Wordpress shortcode to add a custom Lens search feature on the COVERSE website.
-  * Version:           1.0.5
+  * Version:           1.1.3
   * Requires at least: 6.0
   * Requires PHP:      7.0
   * Author:            Rado Faletič
@@ -18,7 +18,7 @@
   * Text Domain:       COVERSE Lens search
   * License:           GPL v2 or later
   * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
-  * Update URI:        https://RadoFaletic.com/plugins/info.json
+  * Update URI:        https://raw.githubusercontent.com/coverseau/profile-lens-search/refs/heads/main/update-info.json
   */
 
 /*
@@ -28,6 +28,47 @@
   
   You should have received a copy of the GNU General Public License along with {Plugin Name}. If not, see {URI to Plugin License}.
   */
+
+/**
+ * Plugin updater handler function.
+ * Pings the Github repo that hosts the plugin to check for updates.
+ */
+if (!function_exists('coverse_lens_search_check_for_plugin_update')) {
+  function coverse_lens_search_check_for_plugin_update($transient) {
+    // If no update transient or transient is empty, return.
+    if (empty($transient->checked)) {
+      return $transient;
+    }
+    
+    // Plugin slug, path to the main plugin file, and the URL of the update server
+    $plugin_slug = 'coverse-lens-search/coverse-lens-search.php';
+    $update_url = 'https://raw.githubusercontent.com/coverseau/coverse-lens-search/refs/heads/main/update-info.json';
+    
+    // Fetch update information from your server
+    $response = wp_remote_get($update_url);
+    if (is_wp_error($response)) {
+      return $transient;
+    }
+    
+    // Parse the JSON response (update_info.json must return the latest version details)
+    $update_info = json_decode(wp_remote_retrieve_body($response));
+    
+    // If a new version is available, modify the transient to reflect the update
+    if (version_compare($transient->checked[$plugin_slug], $update_info->new_version, '<')) {
+      $plugin_data = array(
+          'slug'        => 'coverse-lens-search',
+          'plugin'      => $plugin_slug,
+          'new_version' => $update_info->new_version,
+          'url'         => $update_info->url,
+          'package'     => $update_info->package, // URL of the plugin zip file
+        );
+      $transient->response[ $plugin_slug ] = (object) $plugin_data;
+    }
+    
+    return $transient;
+  }
+  add_filter('pre_set_coverse_lens_search_transient_update_plugins', 'coverse_lens_search_check_for_plugin_update');
+}
 
 if (!function_exists('RadoFaletic_com_check_for_updates')) {
   function RadoFaletic_com_check_for_updates($update, $plugin_data, $plugin_file) {
@@ -52,7 +93,6 @@ if (!function_exists('RadoFaletic_com_check_for_updates')) {
 }
 
 function coverse_lens_search() {
-  
   wp_enqueue_style('coverse-lens-search-style', plugins_url('/css/lens.embed.css', __FILE__), null, false);
   
   global $wp_filesystem;
@@ -68,3 +108,21 @@ function coverse_lens_search() {
   return $content;
 }
 add_shortcode('coverse-lens-search', 'coverse_lens_search');
+
+
+function coverse_lens_attribution() {
+  wp_enqueue_style('coverse-lens-attribution-style', plugins_url('/css/lens.attribution.css', __FILE__), null, false);
+  
+  global $wp_filesystem;
+  require_once(ABSPATH . '/wp-admin/includes/file.php');
+  WP_Filesystem();
+  $htmlTemplateFile = plugin_dir_path( __FILE__ ) . 'coverse-lens-attribution.html';
+  
+  $content = '';
+  if ($wp_filesystem->exists($htmlTemplateFile)) {
+      $content = $wp_filesystem->get_contents($htmlTemplateFile);
+  }
+  
+  return $content;
+}
+add_shortcode('coverse-lens-attribution', 'coverse_lens_attribution');
